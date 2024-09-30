3278a093bd1f87666da1655c10985e6e2283c0cb
Arthur De Mello Vieira <arthur.1522@alunosatc.edu.br>
2024-09-30 20:51:23 -0300

    mm

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert, TextInput} from 'react-native';
 
-export default function App() {
+const TextInputExample = () => {
+  const [number, onChangeNumber] = React.useState('');
+
+  const validacao = () => {
+    const value = parseInt(number);
+
+    if (value === 3) {
+      Alert.alert('Você Acertou o Número!');
+    } else {
+      Alert.alert('Você Errou o Número!');
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Digite um Número"
+        keyboardType="numeric"
+      />
+
+       <Button style={styles.button}
+        title="Clique!"
+        color="#f194ff"
+        onPress={validacao}
+      />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center', 
+    
   },
+  input: {
+    height: 40,
+    width: 400,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }
 });
+
+export default TextInputExample;
